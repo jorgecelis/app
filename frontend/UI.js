1ad6bae4d7cc4a20acae6a3b26414478d4b0f5d3
jorge <celis244@gmail.com>
2020-08-26 01:45:13 -0300

    commit
    otro commit

--- frontend/UI.js
+++ frontend/UI.js
@@ -9,28 +9,29 @@ class UI {
     const books = await bookService.getBooks();
     const booksCardContainer = document.getElementById('books-cards');
     booksCardContainer.innerHTML = '';
-    books.forEach((book) => {
+    books.forEach(book => {
       const div = document.createElement('div');
       div.className = 'animated fadeInRight';
       div.innerHTML = `
       <div class="card m-2">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-                <img src="${book.imagePath}" class="img-fluid" alt="">
-            </div>
-            <div class="col-md-8">
-                <div class="card-block px-2">
+            <div class="row no-gutters">
+                  <div class="col-md-4">
+                      <img src="${book.imagePath}" class="img-fluid" alt="">
+                  </div>
+                <div class="col-md-8">
+                  <div class="card-block px-2">
                     <h4 class="card-title">${book.title}</h4>
                     <p class="card-text">${book.author}</p>
                     <a href="#" class="btn btn-danger delete" _id="${book._id}">X</a>
+                  </div>
                 </div>
-            </div>
-        </div>
+              </div>
         <div class="card-footer w-100 text-muted">
           ${format(book.created_at)}
         </div>
       </div>
-      `;
+      `
+      ;
       booksCardContainer.appendChild(div);
     });
   }
